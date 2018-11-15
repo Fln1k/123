4ffb8137f590cefddd2b16687a892d135a1fc99c
Sergei <sergeitrigubovfleps@gmail.com>
2018-11-16 00:37:51 +0300

    new files

---
+++ Assets/Scripts/CardManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Card
+{
+    public string Name;
+    public Sprite Logo;
+    public int Attack;
+    public int Defense;
+
+    public Card(string name,string logoPath,int attack,int defense)
+    {
+        Name = name;
+        Logo = Resources.Load<Sprite>(logoPath);
+        Attack = attack;
+        Defense = defense;
+    }
+}
+
+public static class CardManager
+{
+    public static List<Card> AllCards = new List<Card>();
+}
+
+public class CardManagerScript : MonoBehaviour
+{
+    public void Awake()
+    {
+        CardManager.AllCards.Add(new Card("Guard", "Images/Guard", 1, 2));
+        CardManager.AllCards.Add(new Card("Bat", "Images/Bat", 2, 1));
+        CardManager.AllCards.Add(new Card("Werewolf", "Images/Werewolf", 2, 2));
+        CardManager.AllCards.Add(new Card("Archer", "Images/Archer", 2, 2));
+        CardManager.AllCards.Add(new Card("Dragon", "Images/GoldDragon", 8, 8));
+    }
+
+}
---
+++ Assets/Scripts/DropPlaceScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum FieldType
+{
+    Self_Hand,
+    Self_Field,
+    Enemy_Hand,
+    Enemy_Field
+}
+
+public class DropPlaceScript : MonoBehaviour, IDropHandler,IPointerEnterHandler,IPointerExitHandler
+{
+    public FieldType Type;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (Type != FieldType.Self_Field)
+            return;
+        CardMovementScript card = eventData.pointerDrag.GetComponent<CardMovementScript>();
+        if (card)
+            card.DefaultParent = transform;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null || Type==FieldType.Enemy_Field || Type == FieldType.Enemy_Hand)
+            return;
+
+        CardMovementScript card = eventData.pointerDrag.GetComponent<CardMovementScript>();
+
+        if (card)
+            card.DefaultTempCardParent = transform;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        CardMovementScript card = eventData.pointerDrag.GetComponent<CardMovementScript>();
+
+        if (card && card.DefaultTempCardParent == transform)
+            card.DefaultTempCardParent = card.DefaultParent;
+    }
+}
